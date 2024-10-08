66ac7189812bcfe748f378d2f3098803e8617efa
3090 <3090@noel.com>
2024-10-08 08:57:51 +0800

    2024 1008

---
+++ init_port.sh
@@ -0,0 +1,8 @@
+# kill 5001/8500/8501/8502/8602/8702
+ports=(5001 8500 8501 8502 8602 8702 8801 8802 8901)
+for port in "${ports[@]}"
+do
+kill $(lsof -i:$port | awk '{print $2}' | awk 'NR==2{print}')
+done
+
+python app.py
